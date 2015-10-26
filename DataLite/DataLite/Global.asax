0606b7b0e3e67610025df9d7fb0c5b5aa2ccb6d1
Kollrich14 <christian.ko11er@hotmail.com>
2015-10-26 20:03:18 +0100

    Default WebForm erstellt.

---
+++ DataLite/DataLite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataLite.Global" Language="C#" %>
---
+++ DataLite/DataLite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataLite.Startup))]
+namespace DataLite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
